c247fc69c3923b2464ef653d348f02acf7efdd7d
PizzaBox <18346187+PizzaBox@users.noreply.github.com>
2020-04-06 05:09:06 -0400

    Update imageOnSave.jsx
    Reworked how the script works.
    
     It now only produces .jpg images because large canvas' produce mammoth size .png files (5000x3000 @ 72dpi is 52MB/image).
    
    Also changed activation method. Checks for a layer called "imageonsave" and if it's visible. If so, it captures

--- imageOnSave.jsx
+++ imageOnSave.jsx
@@ -1,50 +1,46 @@
 /*
-@@@BUILDINFO@@@ imageOnSave.jsx 1.0.1
+@@@BUILDINFO@@@ imageOnSave.jsx 1.1.0
 */
 
-var begDesc = "$$$/JavaScripts/imageOnSave/Description=Saves an image of the active document to a folder in the same directory. Will only activate when .psd files are saved. RGB colour mode will save a .png, others will save a .jpg (12 quality)." // endDesc
-var begName = "$$$/JavaScripts/imageOnSave/MenuName=imageOnSave-v1.0.1" // endName
+var begDesc = "$$$/JavaScripts/imageOnSave/Description=Saves an .jpg of the saved document to a folder in the same directory. This will only trigger when a layer named \"imageonsave\" is present, and is visible." // endDesc
+var begName = "$$$/JavaScripts/imageOnSave/MenuName=imageOnSave-v1.1.0" // endName
 
 try {
   var activeDocument = app.activeDocument
   var documentName = activeDocument.name
-  var lastDotIndex = documentName.lastIndexOf('.')
-  var fileType =  documentName.substr(lastDotIndex)
-  
-  if (fileType == '.psd') {
-    var fileName = documentName.substr(0, lastDotIndex) || documentName
-    var saveOptions = saveOptions(activeDocument.mode)
+
+  if (hasActivationLayer(activeDocument) == true) {
+    var fileName = documentName.substr(0, documentName.lastIndexOf('.')) || documentName
     var saveFolder = activeDocument.path + '/' + fileName + '-imageOnSave'
 
     var createdOutputFolder = createOutputFolder(saveFolder)
-    if (createdOutputFolder == false){
+    if (createdOutputFolder == false) {
       showAlert('Failed to create output folder. Image was not saved.', true)
-    } else{
-      var savePath = saveFolder + '/' + fileName + '-' + String(Date.now()) + saveOptions.ext
-      saveImage(activeDocument, savePath, saveOptions.options)
+    } else {
+      var savePath = saveFolder + '/' + fileName + '-' + String(Date.now()) + '.jpg'
+      try {
+    		activeDocument.saveAs(new File(savePath), new JPEGSaveOptions({ quality: 12 }), true, Extension.LOWERCASE) 
+  		} catch (e) {
+    		showAlert('Failed to save image', true)
+  		}
     }
   } 
 } catch (e) {
   showAlert ('Whoops something broke', true)
 }
 
-function saveImage(activeDocument, savePath, saveOptions){
-  try {
-    activeDocument.saveAs(new File(savePath), saveOptions, true, Extension.LOWERCASE) 
-  } catch (e){
-    showAlert('Failed to save image', true)
-  }
+function hasActivationLayer(doc) {
+	var layers = doc.layers
+	for (var i=0;i<layers.length;i++){	
+		var layer = layers[i]
+		if (layer.name.toLowerCase() == 'imageonsave' && layer.visible == true) {
+			return true
+		}
+	}
+	return false
 }
 
-function saveOptions(mode){
-  if (mode != 'DocumentMode.RGB'){
-    return { ext:'.jpg', options: new JPEGSaveOptions({ quality: 12 }) }
-  } else {
-    return { ext:'.png', options: new PNGSaveOptions() }
-  }  
-}
-
-function createOutputFolder(saveFolder){
+function createOutputFolder(saveFolder) {
   var outputFolder = new Folder(saveFolder)
   if (outputFolder.exists == false) {
     var created = outputFolder.create()
@@ -56,7 +52,7 @@ function createOutputFolder(saveFolder){
 }
 
 function showAlert(message, showError){
-  if (showError != true){
+  if (showError != true) {
     showError = false
   }
   alert(message, 'imageOnSave', showError)
